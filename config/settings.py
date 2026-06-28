ea24d5bdb1671a0c346040f6789f180659e5b688
Izk-123 <109396886+Izk-123@users.noreply.github.com>
2026-06-28 03:34:24 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ INSTALLED_APPS = [
     'unfold',
     'unfold.contrib.filters',
     'unfold.contrib.forms',
-
+    
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,7 +29,6 @@ INSTALLED_APPS = [
     'django_filters',
     'django_htmx',
     'axes',
-    'daphne',
     'channels',
 
     'notifications',
