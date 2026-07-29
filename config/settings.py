eee0b6fa7b0a2b562a6c2560332e4fddab15d6de
Izk-123 <109396886+Izk-123@users.noreply.github.com>
2026-07-29 19:55:22 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ── Security Settings ──────────────────────────────────
 SECRET_KEY = config('SECRET_KEY')
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # ───────────────────────────────────────────────────────────────
